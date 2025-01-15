dadfd30289d6d6adb29f7ba9a4d88838d0822780
Berhan <mberhankokum@gmail.com>
2025-01-15 19:53:03 +0300

    env edit

--- src/appwrite.js
+++ src/appwrite.js
@@ -5,7 +5,7 @@ const client = new Client();
 client
     .setEndpoint('https://cloud.appwrite.io/v1') // Appwrite sunucu URL'si
     .setProject('6786aee6002faa7794b1');     // Appwrite proje ID'si
-
+    
 export const account = new Account(client);
 export const databases = new Databases(client);
 export const query = new Query(client);
